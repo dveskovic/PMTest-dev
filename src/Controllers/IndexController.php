93179936ca98f7a81ea4f1c0418c85b738919cb6
n2go <n2go@logeecom.com>
2016-12-26 15:48:14 +0100

    commit1

---
+++ src/Controllers/IndexController.php
@@ -0,0 +1,61 @@
+<?php
+namespace PMTest\Controllers;
+
+use Plenty\Plugin\Controller;
+use Plenty\Plugin\ConfigRepository;
+use Plenty\Plugin\Templates\Twig;
+use Plenty\Modules\Frontend\Services;
+use Plenty\Modules\System\Models;
+use Plenty\Plugin\Http\Request;
+use Plenty\Plugin\Http\Response;
+use IO\Services\ItemService;
+use IO\Helper\TemplateContainer;
+/**
+ * Class ContentController
+ * @package PMTest\Controllers
+ */
+class IndexController extends Controller
+{
+
+    /**
+     * @var null|Request
+     */
+    private $request = null;
+    /**
+     * @var null|Response
+     */
+    private $response = null;
+
+    public function __construct(
+        Request $request,
+        Response $response)
+    {
+        $this->request = $request;
+        $this->response = $response;
+    }
+
+    /**
+     * @param Twig $twig
+     * @return string
+     */
+    public function execute(Twig $twig, Models\WebstoreConfiguration $designConfig):string
+    {
+        $productIds	= $this->request->get('productIds');
+
+        //$productIds = isset($productIds) ? explode(',', $productIds) : null;
+
+        //$currentItem = $itemService->getItem($itemId);
+        // $products = [];
+        /*	$products[] = [
+                'id' => $product->getId(),
+                'link' => $product->getUrlModel()->getUrl($product),
+                'price' => $priceHelper->currency($product->getFinalPrice(), true, false),
+                'image' => ($image ? $helper->getMediaUrl($image) : ($thumbnailHolder ? $placeholderPath : null)),
+                'title' => $product->getName(),
+            ];*/
+
+        $test = ['test' => $productIds];
+
+        return $this->response->json(json_encode($test));
+    }
+}
